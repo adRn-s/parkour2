e26f8a91f406c4f0b7964dd51edf1d1fd5aee546
Saurabh Dome <domesaurabh@gmail.com>
2024-11-08 01:54:55 +0100

    ExtJS Vue finally working together :')

---
+++ backend/static/main-hub/app/view/incominglibrariesvue/IncomingLibrariesVue.js
@@ -0,0 +1,20 @@
+Ext.define("MainHub.view.incominglibrariesvue.IncomingLibrariesVue", {
+  extend: "Ext.container.Container",
+  xtype: "incoming-libraries-vue",
+
+  layout: "fit",
+
+  initComponent: function () {
+    this.callParent(arguments);
+
+    var homeUrl = window.location.origin;
+
+    this.add({
+      xtype: "component",
+      html:
+        '<iframe src="' +
+        homeUrl +
+        '/vue/incoming_libraries_samples" width="100%" height="100%" frameborder="0"></iframe>',
+    });
+  },
+});
